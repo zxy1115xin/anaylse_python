0c53237fa67609f26b6169061c70b966b720b21f
zxy1115xin <111327097+zxy1115xin@users.noreply.github.com>
2024-10-20 13:44:49 +0800

    批量处理

--- script/try.py
+++ script/try.py
@@ -3,36 +3,41 @@ from sklearn.decomposition import NMF
 import scipy.io
 import matplotlib.pyplot as plt
 
-# 读取 .mat 文件
-mat_data = scipy.io.loadmat('.//data//test.mat')
-data_array = mat_data['angle_matrix_unit']
-data_array[data_array < 0] = 0
-
 # 设置 NMF 模型
 n_components = 3  # 分解后的矩阵的潜在特征数
 model = NMF(n_components=n_components, init='random', random_state=0, l1_ratio=0.16,alpha_W=0.0012)
 
-# 进行矩阵分解，得到矩阵 W 和 H
-W = model.fit_transform(data_array )
-H = model.components_
 
+# 读取 .mat 文件
+mat_data = scipy.io.loadmat('.//data//moment_matrix.mat')
+moment_out={}
+# print("Keys in the MAT file:", mat_data.keys())
+for num in range(8):
+    data_array = mat_data[f'moment_{num+1}']
+    data_array[data_array < 0] = 0
 
-plt.figure(figsize=(10, 6))
-plt.plot(W[:, 0], label='Column 1', linewidth=2)
-plt.plot(W[:, 1], label='Column 2', linewidth=2)
-plt.plot(W[:, 2], label='Column 3', linewidth=2)
-plt.title('Synergy')
-plt.xlabel('Gait cycle (%)')
-plt.ylabel('Y')
-plt.grid(True)
-plt.legend()
-plt.show()
+    # 进行矩阵分解，得到矩阵 W 和 H
+    W = model.fit_transform(data_array )
+    H = model.components_
 
-# 将这些数据保存到 .mat 文件中
-data_dict = {
-    'matrix': W,
-    'vector': H,
-}
+    # 绘图
+    plt.figure(figsize=(10, 6))
+    plt.plot(W[:, 0], label='Column 1', linewidth=2)
+    plt.plot(W[:, 1], label='Column 2', linewidth=2)
+    plt.plot(W[:, 2], label='Column 3', linewidth=2)
+    plt.title('Synergy')
+    plt.xlabel('Gait cycle (%)')
+    plt.ylabel('Y')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
 
-scipy.io.savemat('multiple_data.mat', data_dict)
-
+    # 将这些数据保存到 .mat 文件中
+    data_dict = {
+        f'Principal_{num+1}': W,
+        f'Weight_{num+1}': H,
+    }
+    moment_out.update(data_dict)
+    
+scipy.io.savemat('multiple_data.mat',  moment_out)
+print( moment_out.keys())
